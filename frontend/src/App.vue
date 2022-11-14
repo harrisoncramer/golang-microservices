f3dd9921ec3547f23170997216959be8ba9e83ba
Harrison Cramer <harrisoncramer@gmail.com>
2022-11-13 22:41:10 -0500

    Added basic vue setup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,53 +1,54 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
 import { ref } from 'vue'
 
-const brokerData = ref(null)
-const brokerErr = ref(null)
+const brokerResponse = ref<string>()
+const brokerErr = ref<string>()
+const brokerResponseCount = ref<number>(0)
 
 async function handleTestBroker () {
   const body = { method: 'POST' }
 
   try {
+    brokerResponseCount.value += 1
     const response = await fetch("http://localhost:8080", body)
     const json = await response.json()
-    brokerData.value = json
+    brokerResponse.value = json
+
+    if(json.error) {
+      brokerErr.value = json.message
+    } else {
+      brokerResponse.value = json.message
+    }
   } catch (err) {
-    // brokerErr.value = err
+    brokerErr.value = 'Could not reach broker'
   }
 }
 
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <button @click="handleTestBroker">Test Broker</button>
-  <br>
-  <div>
-    {{ JSON.stringify(brokerData) }}
+  <div class="test broker-test">
+    <button @click="handleTestBroker">Test Broker</button>
+    <div>
+      Response: {{ brokerResponse }}
+    </div>
+    <div>
+      Error: {{ brokerErr ?? "No error" }}
+    </div>
+    <div v-if="brokerResponseCount">
+      API Calls: {{ brokerResponseCount }}
+    </div>
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+.test {
+  gap: 1em;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.broker-test {
+  display: flex;
+  align-items: center;
+  justify-content: start;
 }
 </style>
