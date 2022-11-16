dfc665437aead7ccdafe77550a46b2f299b7df53
Harrison Cramer <harrisoncramer@gmail.com>
2022-11-16 07:50:51 -0500

    Starting to set up logger service with MongoDB

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	grpcPort = "50001"
+	mongoUrl = "mongodb://mongo:27017"
+)
+
+var client *mongo.Client
+
+type Config struct {
+}
+
+func main() {
+	// Connect to MongoDB
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client = mongoClient
+
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI(mongoUrl)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error Connecting to MongoDB", err)
+		return nil, err
+	}
+
+	return c, nil
+}
